d8539dbedf8c2e8891fcc56e49ccf9c3a4a84558
huku3 <mhideyuki7777@icloud.com>
2025-08-30 12:12:42 +0900

    返答メッセージ修正

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    send_message = datetime.now()
+    send_message = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=send_message))
 
